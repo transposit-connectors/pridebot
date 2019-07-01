1e7674360190373e6ff5d911bd784f1f56c070df
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-01 16:17:56 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -2,7 +2,7 @@
   let qs = require('qs.js');
   let body = qs.parse(http_event.parsed_body);
   let recipient = body.From;
-  let event = api.run('this.get_event', {location: body.Body.trim()});
+  let event = api.run('this.get_event', {location: body.Body.trim().toLowerCase()});
   if (event.length < 1 || event == undefined) {
     let message = 'Sorry, no events in that city. 😞\n\nFrom your friends at transposit.com';
     api.run('this.send_message', {recipient: recipient, message: message});
