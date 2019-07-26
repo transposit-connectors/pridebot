285f4ef641a3ff3c38ae478415dda26978bab9ee
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-26 18:02:49 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -4,11 +4,10 @@
   let event = api.run('this.get_event', {location: body.Body.trim().toLowerCase()});
   if (event.length < 1 || event == undefined) {
     let message = 'Sorry, no events in that city. 😞\n\nFrom your friends at transposit.com';
-    api.run('this.send_message', {recipient: recipient, message: message});
-  }
-  else {
+    api.run('this.send_message', {recipient: recipient, message: message, sid: env.get("sid"), phonenum: env.get("phonenum")});
+  } else {
     let message = `🏳️‍🌈 ${event[0].title}, ${event[0].date}: ${event[0].url} 🏳️‍🌈\n\nFrom your friends at transposit.com`;
-    api.run('this.send_message', {recipient: recipient, message: message});
+    api.run('this.send_message', {recipient: recipient, message: message, sid: env.get("sid"), phonenum: env.get("phonenum")});
   }
   return { status_code: 200 };
 }
